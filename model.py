bbd21af55483d8e35de8e96dd63d47ae20a3993f
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-08-20 00:39:15 +0300

    Complete preprocessing part

--- model.py
+++ model.py
@@ -72,10 +72,45 @@ train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
 
 for dataset in combine:
   dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
-  dataset.loc[ dataset['Age'] > 16 and dataset['Age'] <= 32, 'Age'] = 1
-  dataset.loc[ dataset['Age'] > 32 and dataset['Age'] <= 48, 'Age'] = 2
-  dataset.loc[ dataset['Age'] > 48 and dataset['Age'] <= 64, 'Age'] = 3
+  dataset.loc[ (dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+  dataset.loc[ (dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+  dataset.loc[ (dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
   dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
 
 train_df = train_df.drop(['AgeBand'], axis=1)
+combine = [train_df, test_df]
+
+for dataset in combine:
+  dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+for dataset in combine:
+  dataset['IsAlone'] = 0
+  dataset.loc[ dataset['FamilySize'] == 1, 'IsAlone'] = 1
+
+train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+combine = [train_df, test_df]
+
+
+for dataset in combine:
+    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+
+freq_port = train_df.Embarked.dropna().mode()[0]
+for dataset in combine:
+  dataset.Embarked = dataset['Embarked'].fillna(freq_port)
+
+for dataset in combine:
+  dataset['Embarked'] = dataset['Embarked'].map( { 'S': 0, 'C': 1, 'Q': 2 } ).astype(int)
+
+
+
+for dataset in combine:
+  dataset['Fare'].fillna(dataset['Fare'].dropna().median(), inplace=True)
+  dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+  dataset.loc[ (dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+  dataset.loc[ (dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+  dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+  dataset['Fare'] = dataset['Fare'].astype(int)
+  dataset['Title'] = dataset['Title'].astype(int)
+
 combine = [train_df, test_df]
